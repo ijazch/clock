4a7d7e9e0ec9d22940775bcfdb69514d7433dbd3
ijazch <chijaz429@gmail.com>
2023-12-05 00:19:51 +0000

    done

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function setDate() {}
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegree = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getMinutes();
+  const hourDegrees = (mins / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${minsDegrees}deg)`;
+}
 setInterval(setDate, 1000);
